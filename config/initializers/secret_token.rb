17eed8aa7a3ee2ec1547ce2fed6d28b3e477b924
Carlos Cheung <cc653@georgetown.edu>
2013-04-30 21:26:43 -0400

    Initialized Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GradCarloscheung::Application.config.secret_token = 'c1b1237b618733c7e4a84294e580195bb557ab585c46d0fdd725368165500d02e829baf8c3116695471aff2ac876916f97e8d02571ee274eee84be8e3646ff8f'
